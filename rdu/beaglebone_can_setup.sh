0486ea26520d59487e9b111507675315c55ab7df
Sam Cohen <sam@remoracarbon.com>
2021-07-21 19:18:55 +0000

    Initialize RDU Python Module

--- rdu/beaglebone_can_setup.sh
+++ rdu/beaglebone_can_setup.sh
@@ -3,6 +3,6 @@
 # Plug SD card into board, then plug board into computer. Put the BeagleBone Comms Shield onto the device, and properly plug into CAN_High and CAN_Low. Once Windows recognizes the device, you can run 'ssh debian@beaglebone.local`, with password "temppwd". Then, on the BeagleBone, run the below, and if you are connected to an active CAN bus, you should see data print out
 sudo config-pin p9.24 can
 sudo config-pin p9.26 can
-sudo ip link set can1 up type can bitrate 500000
+sudo ip link set can1 up type can bitrate 250000 listen-only on
 sudo ifconfig can1 up
 candump can1
