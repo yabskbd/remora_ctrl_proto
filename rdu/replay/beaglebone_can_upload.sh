8e7061607e07136b92d3d2eb78dce325c089595d
Sam Cohen <samc1213@gmail.com>
2021-08-03 09:56:21 -0400

    Update beaglebone_can_upload.sh

--- rdu/replay/beaglebone_can_upload.sh
+++ rdu/replay/beaglebone_can_upload.sh
@@ -2,4 +2,4 @@
 HOST_NAME=$(cat /etc/hostname);
 HOST_ID=$(echo $HOST_NAME | sed 's/remorabone//g');
 # This absurd pipeline takes files of the form candump<date>.log.gz and uploads them to the cloud in the form candump<date>_<host_id>.log.gz
-find /var/log/candump -type f -name 'candump*.gz' -printf '%f\n' | xargs -I % sh -c "DEST_FILE_NAME=\$(echo % | sed -E \"s/candump(.*).log.gz/candump\1_$HOST_ID.log.gz/g\"); rclone moveto /var/log/candump/% digitalocean:testingcandump/dumps/\$DEST_FILE_NAME"
+find /var/log/candump -type f -name 'candump*.gz' -printf '%f\n' | xargs -I % sh -c "DEST_FILE_NAME=\$(echo % | sed -E \"s/candump(.*).log.gz/candump\1_$HOST_ID.log.gz/g\"); echo \"Uploading %\"; rclone moveto /var/log/candump/% digitalocean:testingcandump/dumps/\$DEST_FILE_NAME"
