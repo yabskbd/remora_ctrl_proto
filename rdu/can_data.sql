5dec03ca903e4ebb5f4aeaeed0e2fa56d3cf8a37
Sam Cohen <samc1213@gmail.com>
2021-08-05 19:49:00 -0400

    Delete can_data.sql

--- rdu/can_data.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE can_data (
-	time TIMESTAMP WITHOUT TIME ZONE NOT NULL,
-	metric varchar(200) NOT NULL,
-	value double PRECISION NOT NULL
-);
-
-
-SELECT create_hypertable('can_data', 'time');
